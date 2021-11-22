c213483889cce527107ce492d5b20afc521c1374
Sleeperbix <66949419+Sleeperbix@users.noreply.github.com>
2021-11-22 19:37:31 +0000

    Add files via upload

---
+++ Salary.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float salary;
+	float newSalary;
+
+	//Using $ instead of £ because of the encoding? Seems to display them as "?"s.
+	cout << "How much money do you earn in a month? : $";
+	cin >> salary;
+	cout << "You earn $" << salary * 12 << " a year.\n";
+	newSalary = salary * 1.1;
+	cout << "With a 10% increase in payrise, you'd earn $" << newSalary << " a month, and you'd earn $" << newSalary * 12 << " a year.\n";
+}
